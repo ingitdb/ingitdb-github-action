c3b7a1914d846259c888faeb495bc70eea69df42
Alexander Trakhimenok <alex@trakhimenok.com>
2021-09-21 01:28:20 +0100

    readJSON(): unknown

--- src/io.ts
+++ src/io.ts
@@ -6,7 +6,7 @@
 import {readFileSync} from 'fs'
 
 // Function to read and parse a JSON
-export function readJSON(filename: string): any {
+export function readJSON(filename: string): unknown {
   const buffer = readFileSync(filename)
   return JSON.parse(buffer.toString())
 }
