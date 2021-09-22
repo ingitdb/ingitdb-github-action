aa2d60cf4719292bdd51d911e0eb496a9d149096
Alexander Trakhimenok <alex@trakhimenok.com>
2021-09-22 13:22:52 +0100

    auto-fix

--- src/collections.ts
+++ src/collections.ts
@@ -1,46 +1,57 @@
-import { readdir } from "fs";
-import { Collection } from "./models";
-import { readJSON, writeJSON } from "./json-io";
+import {readdir} from 'fs'
+import {Collection} from './models'
+import {readJSON, writeJSON} from './json-io'
 
-export async function processCollections(collections: Collection[]): Promise<void> {
+export async function processCollections(
+  collections: Collection[]
+): Promise<void> {
   return new Promise(async (resolve, reject) => {
-    const tasks = collections.map(processCollection);
-    const results = await Promise.all(tasks);     // Gather up the results.
-    resolve();
-  });
+    const tasks = collections.map(processCollection)
+    const results = await Promise.all(tasks) // Gather up the results.
+    resolve()
+  })
 }
 
 async function processCollection(definition: Collection): Promise<void> {
   return new Promise((resolve, reject) => {
-    const records: { [id: string]: unknown } = {};
-    const collection = { definition, records };
-    processCollectionRecords(collection).then(() => {
-      resolve();
-    }).catch(reject);
-  });
+    const records: {[id: string]: unknown} = {}
+    const collection = {definition, records}
+    processCollectionRecords(collection)
+      .then(() => {
+        resolve()
+      })
+      .catch(reject)
+  })
 }
 
 interface ICollection {
-  definition: Collection;
-  records: { [id: string]: unknown };
+  definition: Collection
+  records: {[id: string]: unknown}
 }
 
-async function processCollectionRecords(collection: ICollection): Promise<void> {
+async function processCollectionRecords(
+  collection: ICollection
+): Promise<void> {
   return new Promise<void>((resolve, reject) => {
     readdir(collection.definition.dir, (err, files) => {
       if (err) {
-        reject(err);
-        return;
+        reject(err)
+        return
       }
-      const tasks = files.map(file => processRecordDir(collection, file));
-      Promise.all(tasks).then(() => resolve()).catch(reject);
-    });
-  });
+      const tasks = files.map(async file => processRecordDir(collection, file))
+      Promise.all(tasks)
+        .then(() => resolve())
+        .catch(reject)
+    })
+  })
 }
 
-async function processRecordDir(collection: ICollection, dirPath: string): Promise<unknown> {
-  const jsonPath = dirPath + "/record.json";
-  const record = await readJSON(jsonPath);
-  await writeJSON(record, jsonPath);
-  return Promise.resolve(record);
+async function processRecordDir(
+  collection: ICollection,
+  dirPath: string
+): Promise<unknown> {
+  const jsonPath = `${dirPath}/record.json`
+  const record = await readJSON(jsonPath)
+  await writeJSON(record, jsonPath)
+  return Promise.resolve(record)
 }
--- src/json-sorter.ts
+++ src/json-sorter.ts
@@ -2,14 +2,13 @@
 const replacer = (key: unknown, value: any) =>
   value instanceof Object && !(value instanceof Array)
     ? Object.keys(value)
-      .sort()
-      .reduce((sorted: any, key: string) => {
-        sorted[key] = value[key];
-        return sorted;
-      }, {})
-    : value;
+        .sort()
+        .reduce((sorted: any, key: string) => {
+          sorted[key] = value[key]
+          return sorted
+        }, {})
+    : value
 // Usage: JSON.stringify({c: 1, a: { d: 0, c: 1, e: {a: 0, 1: 4}}}, replacer);
 
-
-export const stringifySorted = (value: any) => JSON.stringify(value, replacer, 2);
-
+export const stringifySorted = (value: any) =>
+  JSON.stringify(value, replacer, 2)
